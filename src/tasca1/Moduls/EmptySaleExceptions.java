d09513f5b44aaf2926182cd5ed4a11db96bebb39
AngelPelaezSanz <angelpelaezs11@gmail.com>
2024-11-25 11:38:30 +0100

    Exercici 1

---
+++ src/tasca1/Moduls/EmptySaleExceptions.java
@@ -0,0 +1,10 @@
+package tasca1.Moduls;
+
+public class EmptySaleExceptions extends Exception{
+    public EmptySaleExceptions(){
+        super ("To make a sale you first need to add products");
+
+    }
+
+
+}
